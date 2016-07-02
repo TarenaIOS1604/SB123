b666546640f5802bd3d9af7c81c3fe239a3d5ed6
wiley <635835286@qq.com>
2016-07-02 14:46:38 +0800

    2016-07-02
    创建项目

---
+++ SB123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SB123
+//
+//  Created by Wiley on 16/7/2.
+//  Copyright © 2016年 Wiley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SB123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SB123
+//
+//  Created by Wiley on 16/7/2.
+//  Copyright © 2016年 Wiley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
